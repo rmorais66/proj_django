d5e0aa80501d4d6b67a93bd1c630b0200dcb0dc8
rodolfo <rmorais66@gmail.com>
2026-09-18 19:35:38 -0300

    removendo coisas inuteis

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from recipes.views import sobre, home, contato
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+
 ]
